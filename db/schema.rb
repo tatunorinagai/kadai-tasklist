17458fd8a563a2f8b02dea794a7acdf1d6ca933f
tatu <tatu@>
2020-08-29 12:23:55 +0000

    status

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_22_012327) do
+ActiveRecord::Schema.define(version: 2020_08_29_113352) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
